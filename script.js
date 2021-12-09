5a5cff317adfafbf08e7fa10ac38829022c95249
Gilbert Zhuo <gilbertzhuo@gmail.com>
2021-12-09 18:07:22 +0800

    update santa

---
+++ script.js
@@ -0,0 +1,18 @@
+const keys = document.querySelectorAll('.key')
+
+for (let i = 0; i < keys.length; i++) {
+    keys[i].addEventListener('click', function() {
+        playNote(keys[i])
+    })
+}
+
+function playNote(key) {
+    const noteAudio = document.getElementById(key.dataset.note)
+    noteAudio.currentTime = 0
+    noteAudio.play()
+    key.classList.add('active')
+    setTimeout(() => {
+        key.classList.remove('active')
+    }, 100)
+
+}
