114a32a5dd02bbac28d66b91c3635e96d440bcc5
Gilbert Zhuo <gilbertzhuo@gmail.com>
2021-12-10 10:51:56 +0800

    final

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
 const keys = document.querySelectorAll('.key')
 
 for (let i = 0; i < keys.length; i++) {
@@ -5,6 +7,22 @@ for (let i = 0; i < keys.length; i++) {
         playNote(keys[i])
     })
 }
+
+const whiteKeys = document.querySelectorAll('.key.white')
+const blackKeys = document.querySelectorAll('.key.black')
+
+document.addEventListener('keydown', e => {
+    if (e.repeat) return
+    const key = e.key;
+    const whiteKeyIndex = WHITE_KEYS.indexOf(key)
+    const blackKeyIndex = BLACK_KEYS.indexOf(key)
+    if (whiteKeyIndex > -1) {
+        playNote(whiteKeys[whiteKeyIndex])
+    }
+    if (blackKeyIndex > -1) {
+        playNote(blackKeys[blackKeyIndex])
+    }
+})
 
 function playNote(key) {
     const noteAudio = document.getElementById(key.dataset.note)
